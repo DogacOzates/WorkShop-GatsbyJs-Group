eda4580bdc59d4c7011160619c619313fa6aed10
DogacOzates <67386086+DogacOzates@users.noreply.github.com>
2023-05-05 14:29:26 -0700

    first component

---
+++ src/components/CodeSection/CodeSection.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import "./CodeSection.css"
+
+const CodeSection = ({code}) => {
+  return (
+    <div className="codeSection">{code}</div>
+  )
+}
+
+export default CodeSection
