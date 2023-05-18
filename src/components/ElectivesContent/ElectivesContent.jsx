8d62e728ce61800828be766d61fc2dc12a8ee250
koshirok096 <33566624+koshirok096@users.noreply.github.com>
2023-05-17 17:01:16 -0700

    fixed

--- src/components/ElectivesContent/ElectivesContent.jsx
+++ src/components/ElectivesContent/ElectivesContent.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import { Link } from "gatsby"
 import * as styles from './ElectivesContent.module.css';
 
-const articleContents = [
+const articleContentsLeft = [
   {
     title: "Functional Javascript",
     description: "Learn how to write a functional javascript application using React and Redux",
@@ -100,102 +100,107 @@ const articleContents = [
   }
 ];
 
-const articleContents2 = [
+const articleContentsRight = [
   {
-    title: "Functional Javascript",
-    description: "Learn how to write a functional javascript application using React and Redux",
-    code: "npm install -g functional-javascript-workshop",
+    title: "Shader School",
+    description: "Learn the fundamentals of graphics programming using GLSL shaders.",
+    code: "npm install -g shader-school",
   },
   {
-    title: "Level Me Up Scotty!",
-    description: "Learn to use leveldb, a simple key/value store with a vibrant package.",
-    code: "npm install -g levelmeup",
+    title: "Bytewiser",
+    description: "Learn how to manipulate binary data in node.js and HTML5 browsers.",
+    code: "npm install -g bytewiser",
   },
   {
-    title: "ExpressWorks",
-    description: "Learn the basics of the Express.js framework.",
-    code: "npm install -g expressworks",
+    title: "Browserify Adventure",
+    description: "Use npm modules and node-style require() in the browser with browserify.",
+    code: "npm install -g browserify-adventure",
   },
   {
-    title: "Make Me Hapi",
-    description: "Learn all about hapi through a series of challenges.",
-    code: "npm install -g makemehapi",
+    title: "Intro to WebGL",
+    description: "Get started with three.js and WebGL.",
+    code: "npm install -g introtowebgl",
   },
   {
-    title: "Promise It Won't Hurt",
-    description: "Learn to use promises in JavaScript to handle async operations.",
-    code: "npm install -g promise-it-wont-hurt",
+    title: "Count to 6",
+    description: "Learn how to use some features from ES6, the next version of JavaScript.",
+    code: "npm install -g count-to-6",
   },
   {
-    title: "Async You",
-    description: "Learn to use the async package.",
-    code: "npm install -g async-you",
+    title: "Kick off Koa",
+    description: "Getting started with Koa, the next generation web framework for Node.js.",
+    code: "npm install -g kick-off-koa",
   },
   {
-    title: "NodeBot Workshop",
-    description: "Make robots with the johnny-five api.",
-    code: "npm install -g nodebot-workshop",
+    title: "LololoDash",
+    description: "Learn Lo-Dash (fork of underscore) to handle your arrays and objects simple!",
+    code: "npm install -g lololodash",
   },
   {
-    title: "Going Native",
-    description: "An exploration of Node.js from the underside: native C++ add-ons.",
-    code: "npm install -g goingnative",
+    title: "learnyoucouchdb",
+    description: "Learn about CouchDB - the database that completely embraces the web",
+    code: "npm install -g learnyoucouchdb",
   },
   {
-    title: "Planet Proto",
-    description: "Understanding JavaScript Prototypes",
-    code: "npm install -g planetproto",
+    title: "learnuv",
+    description: "Learn uv for fun and profit, a self guided workshop to the library that powers Node.js.",
+    code: "npm install -g learnuv",
   },
   {
-    title: "WebGL Workshop",
-    description: "Learn the basics of WebGL in small, manageable chunks.",
-    code: "npm install -g webgl-workshop",
+    title: "Learn Generators",
+    description: "An Intro to JavaScript ES6 Generators.",
+    code: "npm install -g learn-generators",
   },
   {
-    title: "ESNext Generation",
-    description: "Intro to ES6 Iterators, their use, and how they relate to Generators.",
-    code: "npm install -g esnext-generation",
+    title: "learnyoureact",
+    description: "Let's learn React.js and server side rendering!",
+    code: "npm install -g learnyoureact",
   },
   {
-    title: "Tower of babel",
-    description: "Show you through a series of exercises that introduce you to ES6 features.",
-    code: "npm install -g tower-of-babel",
+    title: "perfschool",
+    description: "Find your way through the web performance optimization maze!",
+    code: "npm install -g perfschool",
   },
   {
-    title: "learnyoumongo",
-    description: "Getting started with MongoDB and Node.js",
-    code: "npm install -g learnyoumongo",
+    title: "Web Audio School",
+    description: "Learn the Web Audio API by completing a series of interactive lessons with a focus on music.",
+    code: "npm install -g web-audio-school",
   },
   {
-    title: "regex-adventure",
-    description: "Parse text with regular expressions",
-    code: "npm install -g regex-adventure",
+    title: "torrential",
+    description: "A set of lessons to show you how to create a simple p2p bittorrent network",
+    code: "npm install -g torrential",
   },
   {
-    title: "learn-sass",
-    description: "Learn the basics of SASS",
-    code: "npm install -g learn-sass",
+    title: "Thinking in React",
+    description: "Learn React's philosophy by building a UI from scratch.",
+    code: "npm install -g thinking-in-react",
   },
   {
-    title: "Pattern Lab Workshop",
-    description: "Learn the basics of Pattern Lab.",
-    code: "npm install -g pattern-lab-workshop",
+    title: "Post-mortem debugging",
+    description: "Learn how to use post-mortem debugging on SmartOS to efficiently debug Node.js applications in production.",
+    code: "npm install -g node-debug-school",
   },
   {
-    title: "learnyoubash",
-    description: "Learn you how to use the terminal and write your first Bash script.",
-    code: "npm install -g learnyoubash",
+    title: "Seneca in practice",
+    description: "Learn how to leverage SenecaJs microservice toolkit.",
+    code: "npm install -g seneca-in-practice",
   },
   {
-    title: "Currying in JavaScript",
-    description: "Learn currying concept and creating a currying function in JavaScript.",
-    code: "npm install -g currying-workshopper",
+    title: "LESS is more",
+    description: "Learn the fundamentals of the LESS CSS preprocessor.",
+    code: "npm install -g less-is-more",
   },
   {
-    title: "bacon-love",
-    description: "Learn concepts of Functional and Reactive Programming using the Bacon.js library.",
-    code: "npm install -g bacon-love",
+    title: "JavaScript best practices",
+    description: "Learn the best practices of writing clean JavaScript code.",
+    code: "npm install -g js-best-practices",
   },
+  {
+    title: "Scope Chains & Closures",
+    description: "Learn the details of Scope, Scope Chains, Closures, and Garbage Collection.",
+    code: "npm i @workshoppers/scope-chains-closures -g",
+  }
 
 ];
 
@@ -204,7 +209,7 @@ function ElectivesHeadline() {
   return (
     <>
     <div className={styles.third}>
-    {articleContents.map((elem, index) => (
+    {articleContentsLeft.map((elem, index) => (
       <div className={styles.elective_contentwrapper}>
         <h4>
           <Link to="#">{elem.title}</Link>
@@ -215,7 +220,7 @@ function ElectivesHeadline() {
     ))}
     </div>
     <div className={styles.third}>
-    {articleContents2.map((elem, index) => (
+    {articleContentsRight.map((elem, index) => (
       <div className={styles.elective_contentwrapper}>
         <h4>
           <Link to="#">{elem.title}</Link>
